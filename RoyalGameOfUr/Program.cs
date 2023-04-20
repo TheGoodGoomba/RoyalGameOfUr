f6cf90aa2e0e7a3f0f9c16951e7600c2e83b2d12
TheGoodGoomba <alexshemmeld@gmail.com>
2023-04-21 08:27:15 +1000

    Add project files.

---
+++ RoyalGameOfUr/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RoyalGameOfUr
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
