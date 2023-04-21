ace0ac5cbe31c20fc29e31dcb831f7080a2fbf42
TheGoodGoomba <alexshemmeld@gmail.com>
2023-04-21 11:10:14 +1000

    draw new board layout

--- RoyalGameOfUr/Board.cs
+++ RoyalGameOfUr/Board.cs
@@ -12,44 +12,44 @@ namespace RoyalGameOfUr
         static string BoardLayout = File.ReadAllText(BoardTxtPath);
         public static List<Square> Squares = new List<Square>
         {
-            new Square(Coordinate.A1, 10, 1),
-            new Square(Coordinate.A2, 7, 1),
-            new Square(Coordinate.A3, 4, 1),
-            new Square(Coordinate.A4, 1, 1),
-            new Square(Coordinate.B1, 10, 3),
-            new Square(Coordinate.B2, 7, 3),
-            new Square(Coordinate.B3, 4, 3),
-            new Square(Coordinate.A1, 1, 3),
-            new Square(Coordinate.C1, 1, 2),
-            new Square(Coordinate.C2, 4, 2),
-            new Square(Coordinate.C3, 7, 2),
-            new Square(Coordinate.C4, 10, 2),
-            new Square(Coordinate.C5, 13, 2),
-            new Square(Coordinate.C6, 16, 2),
-            new Square(Coordinate.C7, 19, 2),
-            new Square(Coordinate.C8, 22, 2),
-            new Square(Coordinate.A5, 22, 1),
-            new Square(Coordinate.A6, 19, 1),
-            new Square(Coordinate.B5, 22, 3),
-            new Square(Coordinate.B6, 19, 3)
+            new Square(Coordinate.A1, 14, 1),
+            new Square(Coordinate.A2, 10, 1),
+            new Square(Coordinate.A3, 6, 1),
+            new Square(Coordinate.A4, 2, 1),
+            new Square(Coordinate.B1, 14, 5),
+            new Square(Coordinate.B2, 10, 5),
+            new Square(Coordinate.B3, 6, 5),
+            new Square(Coordinate.B4, 2, 5),
+            new Square(Coordinate.C1, 2, 3),
+            new Square(Coordinate.C2, 6, 3),
+            new Square(Coordinate.C3, 10, 3),
+            new Square(Coordinate.C4, 14, 3),
+            new Square(Coordinate.C5, 18, 3),
+            new Square(Coordinate.C6, 22, 3),
+            new Square(Coordinate.C7, 26, 3),
+            new Square(Coordinate.C8, 30, 3),
+            new Square(Coordinate.A5, 30, 1),
+            new Square(Coordinate.A6, 26, 1),
+            new Square(Coordinate.B5, 30, 5),
+            new Square(Coordinate.B6, 26, 5)
         };
 
         public static void DrawBoard()
         {
             Console.WriteLine(BoardLayout);
+            var rosettes = new List<Coordinate>
+            {
+                Coordinate.A4, Coordinate.B4, Coordinate.C4, Coordinate.A6, Coordinate.B6
+            };
             Console.BackgroundColor = ConsoleColor.Magenta;
-            Console.SetCursorPosition(1, 1);
-            Console.Write("__");
-            Console.SetCursorPosition(19, 1);
-            Console.Write("__");
-            Console.SetCursorPosition(19, 3);
-            Console.Write("__");
-            Console.SetCursorPosition(1, 3);
-            Console.Write("__");
-            Console.SetCursorPosition(13, 2);
-            Console.Write("__");
+            foreach (var r in rosettes)
+            {
+                var square = Squares.Find(x => x.Coordinates == r);
+                Console.SetCursorPosition(square.Left - 1, square.Top);
+                Console.Write(" * ");
+            }
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.SetCursorPosition(0, 4);
+            Console.SetCursorPosition(7, 1);
             Console.WriteLine();
         }
     }
